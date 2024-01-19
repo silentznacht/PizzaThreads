0f763e24fafa5a22499794bb03b919864f1e4eb7
Kevin Escoto <96765154+silentznacht@users.noreply.github.com>
2024-01-19 18:58:34 -0500

    Add files via upload

---
+++ PizzaThreads/PizzaThreads/src/com/pizzathreads/main/services/orders/ToppingsOrder.java
@@ -0,0 +1,12 @@
+package main.services.orders;
+
+public class ToppingsOrder implements Runnable {
+
+    
+    @Override
+    public void run() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'run'");
+    }
+
+}
