33c7b99e86a5902dba047e3068b6cdc3709442b3
Kevin Escoto <96765154+silentznacht@users.noreply.github.com>
2024-02-01 13:44:13 -0500

    Update 0.2 (signup hotfix/json implement soon)

--- PizzaThreads/src/com/pizzathreads/main/account/signup/SignUp.java
+++ PizzaThreads/src/com/pizzathreads/main/account/signup/SignUp.java
@@ -12,34 +12,41 @@ public class SignUp implements Runnable {
     private static String [] organizationArr = {"gmail", "yahoo", "outlook"};
     private static String [] specialChars = {"()", "{}" , "[]", "|", "`", "¬", "¦", "!" ,"£", "$", "%", "^", "&", "*", "<", ">", ":", ";", "#", "~", "_", "-", "+", "=", "@"};
     
-    static void setEmail() {
+    static void setEmail(String emailToJson) {
         boolean validEmail = false;
+        boolean nextMethod = false;
         String email;
         String userEmail;
+
         do {
-            System.out.println("[Email SignUp (Please Enter a Valid Email)]: ");
+            System.out.println("[ Email SignUp (Please Enter a Valid Email) ]: ");
             email = signUpSc.nextLine();
 
                 for (String word : organizationArr) {
                     if (email.contains(word)) {
-                        System.out.println("Test: " + word);
+                       // System.out.println("Test: " + email);
+                        validEmail = true;
+                        nextMethod = true;
                         break;
                     } else {
                         System.out.println("[Err]: Invalid Email, Try Again");
+                        break;
                     }
-                } validEmail = true;
+                } 
         } while (!validEmail);
 
-        userEmail = email;
-        
-        System.out.println(userEmail);
+        // if email created valid => will pass to the next sequence (create password) & username = email(inputted) to be passed later for json convert
+
+            userEmail = email;
+            if (nextMethod) setPw(userEmail, null);
     }
 
-    static void setPw() { // must contain at least one unicode/special character (!, @, #, etc)
+    static void setPw(String emailToJson, String pwToJson) { // must contain at least one unicode/special character (!, @, #, etc)
         boolean validPassword = false;
         boolean specialCharFound = false;
+        boolean nextMethod2 = false;
         String password;
-        String userPassword;
+      
 
         do {
             System.out.println("[Create A Password (must contain a special character [!, @, #, $, etc])]: ");
@@ -48,11 +55,12 @@ public class SignUp implements Runnable {
                 for (String chars : specialChars) {
                     if (password.toLowerCase().contains(chars)) {
                         specialCharFound = true;
+                        nextMethod2 = true;
                         break;
                     } 
                 } 
 
-                if (specialCharFound) {
+                if (specialCharFound) {                           
                     System.out.println("Password: " + password);
                     validPassword = true;
                     break;
@@ -61,18 +69,27 @@ public class SignUp implements Runnable {
                 }
         } while (!validPassword);
 
-        userPassword = password;
-        //System.out.println("Password: " + userPassword);
+        pwToJson = password;
+        if (nextMethod2) jsonConvert(emailToJson, pwToJson);
+    
     }   
     
     static void signUp() { // sign up
-        setEmail();
-        setPw();
+        /*
+         *                              What to next?
+           
+            *  Find a way to workflow the signup sys, if email is valid will pass onto the pw method
+            *  Then pass these inputs to json convert file
+            *  Finally implement all this in run method thread
+         
+        */
+
+        setEmail(null);
     }
 
     static void jsonConvert(String userEmail, String userPassword) { // converts new credentials to json data
         //SignUp signUp = new SignUp(userEmail, password) // email, password
-
+        System.out.println("[UPDATE]: Json implement soon")
     }
 
     public static void main(String[] args) {
